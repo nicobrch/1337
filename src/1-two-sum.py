5e859352c4ea55bffa2df9467a1af87863a135a5
Nico <nicolas.brch@gmail.com>
2025-02-15 21:32:49 -0300

    Solution for exercise 1

---
+++ src/1-two-sum.py
@@ -0,0 +1,15 @@
+# Link: https://leetcode.com/problems/two-sum/
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i, num in enumerate(nums):
+            if target - num in d:
+                return [d[target - num], i]
+            d[num] = i
+        return []
