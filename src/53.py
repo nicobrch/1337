979ea0f33e848b425bebca28e889823a31c20674
Nico <nicolas.brch@gmail.com>
2025-04-25 14:06:38 -0400

    Solution for exercise 53, o(n) optimized

--- src/53.py
+++ src/53.py
@@ -10,10 +10,12 @@ class Solution(object):
             return nums[0]
 
         max_sum = nums[0]
+        curr_sum = 0
         for i in range(length):
-            curr_sum = 0
-            for j in range(i, length):
-                curr_sum = curr_sum + nums[j]
-                max_sum = max(max_sum, curr_sum)
+            if curr_sum < 0:
+                curr_sum = 0
+
+            curr_sum += nums[i]
+            max_sum = max(max_sum, curr_sum)
 
         return max_sum
