845a2ec6e279a68cc465e5964ffecf842eb43342
Nico <nicolas.brch@gmail.com>
2025-04-25 17:55:10 -0400

    Solution for exercise 57

--- src/57.py
+++ src/57.py
@@ -5,36 +5,25 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
-        inserted_intervals = []
-        if not intervals:
-            inserted_intervals.append(newInterval)
-            return inserted_intervals
-        length = len(intervals)
-        merged_interval = intervals[0][:]
-        left, right = intervals[0][:], intervals[length-1][:]
-        left_index, right_index = 0, length-1
+        res = []
+        i = 0
+        ln = len(intervals)
 
-        while intervals[left_index][0] <= newInterval[0]:
-            # left pointer, check if start_i is less or equal to newInterval start_i
-            left = intervals[left_index][:]
-            left_index += 1
+        # add intervals up to a merging point
+        while i < ln and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
 
-        while intervals[right_index][0] > newInterval[1]:
-            # right pointer, check if start_i is greater than or equal to newInterval end_i
-            right_index -= 1
-            right = intervals[right_index][:]
+        # merge intervals
+        while i < ln and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
+        res.append(newInterval)
 
-        if intervals[right_index][1] <= newInterval[1]:
-            merged_interval = [left[0], newInterval[1]]
-        else:
-            merged_interval = [left[0], right[1]]
+        # add resting intervals
+        while i < ln:
+            res.append(intervals[i])
+            i += 1
 
-        for i in range(left_index-1):
-            inserted_intervals.append(intervals[i][:])
-
-        inserted_intervals.append(merged_interval)
-
-        for i in range(right_index+1, length):
-            inserted_intervals.append(intervals[i][:])
-
-        return inserted_intervals
+        return res
