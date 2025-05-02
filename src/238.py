1223d8f76e048f4b3c63661c99a3f3dee49f3350
Nico <nicolas.brch@gmail.com>
2025-05-02 01:13:22 -0400

    Solution for exercise 238

--- src/238.py
+++ src/238.py
@@ -5,20 +5,16 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        answer = []
+        answer = [1] * len(nums)
 
+        prefix = 1
         for i in range(len(nums)):
-            answer.append(1)
+            answer[i] = prefix
+            prefix *= nums[i]
 
-        for i in range(len(nums)):
-            left, right = 1, 1
-
-            while i - left >= 0:
-                answer[i] *= nums[i-left]
-                left += 1
-
-            while i + right < len(nums):
-                answer[i] *= nums[i+right]
-                right += 1
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            answer[i] *= postfix
+            postfix *= nums[i]
 
         return answer
