365dbff6339bb4e57e889031c87735f20c925fd4
Nico <nicolas.brch@gmail.com>
2025-05-07 00:29:24 -0400

    Solution for problem 70 using recursive function

--- src/70.py
+++ src/70.py
@@ -1,25 +1,13 @@
-class BinaryTreeNode:
-    def __init__(self, val: int = 0, left: int = 1, right: int = 2):
-        self.val = val
-        self.left = val + left
-        self.right = val + right
-
-
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
+        def climb(i, one, two):
+            # Base case: if i == n-1, return 'one'
+            if i == n - 1:
+                return one
+            return climb(i + 1, one + two, one)
+
+        # Edge case: n == 0 or n == 1
+        if n == 0 or n == 1:
             return 1
 
-        def bf(node: BinaryTreeNode):
-            if node.val == n:
-                return 1
-            elif node.val > n:
-                return 0
-            else:
-                left_node = BinaryTreeNode(val=node.val + 1)
-                right_node = BinaryTreeNode(val=node.val + 2)
-                return bf(left_node) + bf(right_node)
-
-        node = BinaryTreeNode()
-
-        return bf(node)
+        return climb(0, 1, 1)
