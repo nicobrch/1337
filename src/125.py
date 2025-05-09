11d5396ebd4895846e4d5f7cef3464bf24894717
Nico <nicolas.brch@gmail.com>
2025-05-08 22:17:17 -0400

    Solution for problem 125, less space

--- src/125.py
+++ src/125.py
@@ -1,9 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # remove all non alpha characters
-        alpha = "".join(filter(str.isalnum, s))
-        # pass to lowercase
-        alpha = alpha.lower()
-        rev = "".join(reversed(alpha))
-        # check if is equal to reverse
-        return True if alpha == rev else False
+        l, r = 0, len(s)-1
+
+        while l < r:
+            while l < r and not self.isAlnum(s[l]):
+                l += 1
+
+            while r > l and not self.isAlnum(s[r]):
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
+
+    def isAlnum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
